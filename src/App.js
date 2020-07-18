bab304cce8bf43e8e6cc4c666532b021208b229a
Shubham Singh <shubhsingh01@gmail.com>
2020-07-19 04:10:34 +0530

    Added Switch to the router code

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Footer } from './components'
-import {Nirvana, PageNotFound} from './pages';
+import { Nirvana, PageNotFound } from './pages';
 
 function App() {
   return (
     <main>
       <Router>
-        <Route exact path='/'>
-          <Nirvana />
-          <Footer />
-        </Route>
-        <Route path="*">
-          <PageNotFound />
-        </Route>
+        <Switch>
+          <Route exact path='/'>
+            <Nirvana />
+            <Footer />
+          </Route>
+          <Route path="*">
+            <PageNotFound />
+          </Route>
+        </Switch>
       </Router>
     </main>
   );
